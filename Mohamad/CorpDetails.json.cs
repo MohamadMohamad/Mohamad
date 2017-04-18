b004b8aece5840b32bfdaeb3485ca82db7861a1a
Shawkey <mohamad.shawkey@gmail.com>
2017-04-18 18:37:08 +0200

    sorting franchises based on (sold homes, total commissio, ...)

--- Mohamad/CorpDetails.json.cs
+++ Mohamad/CorpDetails.json.cs
@@ -10,11 +10,12 @@ namespace Mohamad
             base.OnData();
             UpdateFranchiseList();
         }
-        
-        void UpdateFranchiseList()
+
+        void UpdateFranchiseList(string orderProperty = "Name")
         {
             Corporation corp = (Corporation)Data;
-            FranchiseList = Db.SQL<Franchise>("SELECT f FROM Franchise f WHERE ParentCorp = ?", corp);
+            FranchiseList = Db.SQL<Franchise>("SELECT f FROM Franchise f WHERE f.ParentCorp = ? ORDER BY f." + orderProperty, corp);
+
         }
 
         void Handle(Input.AddFranchiseButton addFranchiseButton)
@@ -28,5 +29,27 @@ namespace Mohamad
             });
             UpdateFranchiseList();
         }
+
+        void Handle(Input.HomesSortButton button)
+        {
+            UpdateFranchiseList("SoldHomes");
+        }
+
+        void Handle(Input.TotalCommissionSortButton button)
+        {
+            UpdateFranchiseList("TotalCommission");
+        }
+
+        void Handle(Input.AverageCommissionSortButton button)
+        {
+            UpdateFranchiseList("AverageCommission");
+        }
+
+        void Handle(Input.TrendSortButton button)
+        {
+            UpdateFranchiseList("Trend");
+        }
+
+
     }
 }
